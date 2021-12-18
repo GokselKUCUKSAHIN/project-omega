63a8fb9444c677884ab3ffc1f757d7a109a6abb1
gokselkucuksahin <gokselkucuksahin@gmail.com>
2021-12-18 21:07:00 +0300

    BodySchemaValidation added.

--- server/src/data-acc/epatch.js
+++ server/src/data-acc/epatch.js
@@ -3,6 +3,7 @@ const saveFactory = require("./saver-factory");
 const Process = require("process");
 const isEmpty = require("../utils/is-empty");
 const formatInt = require("../utils/format-int");
+const bodySchemaValidator = require("../utils/body-schema-validate");
 const router = express.Router();
 
 // data-acc/epatch/
@@ -18,8 +19,7 @@ router.post('/', (req, res, next) => {
   try {
     const body = req.body;
     if (isEmpty(body)) return res.sendStatus(404);
-    console.log("body:", body);
-    // TODO Add Schema validation to body.
+    if (!bodySchemaValidator(body)) return res.sendStatus(418); // I'm a tea pod
     const vmid = body.vmid;
     saver(body, (!!vmid ? formatInt(vmid) : "unknown"));
     res.sendStatus(200);
