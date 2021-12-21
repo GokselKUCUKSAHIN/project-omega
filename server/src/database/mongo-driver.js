cf22eff8ccded7d96f504635037b7c68b8128478
Jellybeanci <gokselkucuksahin@gmail.com>
2021-12-22 01:34:30 +0300

    minor changes and sleep.js.

--- server/src/database/mongo-driver.js
+++ server/src/database/mongo-driver.js
@@ -1,18 +1,22 @@
 const {MongoClient} = require("mongodb");
+const onCloseEvent = require("../utils/onclose-event");
 const int = require("../utils/int");
+const sleep = require("../utils/sleep");
 
 class MongodbDriver {
   constructor(url) {
     this.alive = false;
-    this.client = new MongoClient(url);
+    // this.client = new MongoClient(url);
   }
 
   async initialize() {
     if (this.alive) return;
-    await this.client.connect();
+    // await this.client.connect();
+    await sleep(100);
     this.alive = true;
     this._id = `id:${int(Math.random() * 10_000)}`;
     // TODO add to Callback list with _id
+    onCloseEvent.addCallback(this._id, this._autoClose); // TODO TEST THIS not behave as i expected???
   }
 
   openDb(databaseName) {
@@ -39,15 +43,18 @@ class MongodbDriver {
     if (!this.alive) throw Error("Connection is not alive. Connection dead or never initialized.");
   }
 
-  async close() {
+  close() {
     // TODO remove from callback list
-    return await this._autoClose()
+    console.log("Exit called", this._id);
+    onCloseEvent.removeCallback(this._id);
+    return this._autoClose()
   }
 
   /**@private */
-  async _autoClose() {
+  _autoClose() {
+    console.log("ID=",this._id);
     if (this.alive) {
-      await this.client.close();
+      // await this.client.close();
       this.alive = false;
       return true;
     }
@@ -64,10 +71,22 @@ async function mongoDbDriverFactory(url, alive = true) {
 module.exports = mongoDbDriverFactory;
 
 // DEMO
-
+/*
 mongoDbDriverFactory("hello.com").then(driver => {
   const collection = driver.openDb("epatch").openCollection("results");
   collection.find({}).then(findResult => {
     console.log(findResult.toArray());
   })
 });
+*/
+
+
+(async _ => {
+  const driver = await mongoDbDriverFactory("hello.com");
+  console.log(driver.isAlive())
+  console.log("callback: ", onCloseEvent.hasCallback(driver._id));
+
+  console.log(driver.isAlive());
+  console.log("callback: ", onCloseEvent.hasCallback(driver._id));
+})();
+
