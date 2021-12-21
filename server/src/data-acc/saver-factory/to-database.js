b7570b11ff7861179c7e47e65e7017bd8d144799
gokselkucuksahin <gokselkucuksahin@gmail.com>
2021-12-21 19:51:46 +0300

    toDatabase validate added.

--- server/src/data-acc/saver-factory/to-database.js
+++ server/src/data-acc/saver-factory/to-database.js
@@ -1,11 +1,13 @@
+const bodySchemaValidate = require("../../utils/body-schema-validate");
 const undefCheck = require("../../utils/undef-check");
 
 function toDatabase(databaseURI, dbName, collection) {
   undefCheck(databaseURI, "Database URI Undefined.");
   undefCheck(dbName, "Database Name Undefined.");
   undefCheck(collection, "Database Collection Undefined.");
-  return function (result, validate = false) {
-    // if(validate) let PLACE_HOLDER; // TODO validate the result
+  return async function (result, validate = false) {
+    if (validate && !(await bodySchemaValidate(result))) return false;
+
     // TODO Save to Database
     return // TODO response of operation
   }
