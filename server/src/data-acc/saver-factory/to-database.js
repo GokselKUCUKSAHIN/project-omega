bcb3d97656fd5ca052f69cbfaefe8ddcf27c428b
GokselKUCUKSAHIN <gokselkucuksahin@gmail.com>
2021-12-31 14:43:24 +0300

    factory function refactored.

--- server/src/data-acc/saver-factory/to-database.js
+++ server/src/data-acc/saver-factory/to-database.js
@@ -1,6 +1,7 @@
 const {mongoDbDriverFactory} = require("moosedb");
 const bodySchemaValidate = require("../../utils/body-schema-validate");
 const {undefCheck} = require("undef-check");
+const isEmpty = require("../../utils/is-empty");
 const tryCatch = require("../../utils/try-catch");
 
 async function toDatabase(databaseURI, dbName, collectionName) {
@@ -10,29 +11,28 @@ async function toDatabase(databaseURI, dbName, collectionName) {
   const [driver, err] = await tryCatch(mongoDbDriverFactory)(databaseURI);
   if (err) return console.error("MongoDbDriver faced an Error while creating an instance.");
   const collection = driver.db(dbName).get(collectionName);
-  return {
-    /**@param result {Object}
-     * @param validate {boolean}
-     * @return {Promise<boolean>}
-     */
-    dBsaver: async function (result, validate = false) {
-      if (validate && !(await bodySchemaValidate(result))) return false;
-      return collection.insertOne(result);
-    },
-    /**@param dbName {string}
-     * @param collectionName {string}
-     */
-    dBswitch: function (dbName, collectionName) {
-      undefCheck(dbName, "Database Name Undefined.");
-      undefCheck(collectionName, "Database Collection Undefined.");
-      driver.db(dbName).get(collectionName);
-    },
-    /**@return {Promise<boolean>}
-     */
-    dBclose: async function () {
-      return await driver.close();
-    }
+  return async function (document, validate = false) {
+    undefCheck(document, "Documents cannot be undefined or null.");
+    if (isEmpty(document)) throw Error("Document cannot be empty.");
+    if (validate && !(await bodySchemaValidate(result))) return false;
+    collection.insertOne(document).then(console.log);
   }
 }
 
 module.exports = toDatabase;
+
+/*
+dBsaver: async function (result, validate = false) {
+  if (validate && !(await bodySchemaValidate(result))) return false;
+  return collection.insertOne(result);
+},
+dBswitch: function (dbName, collectionName) {
+  undefCheck(dbName, "Database Name Undefined.");
+  undefCheck(collectionName, "Database Collection Undefined.");
+  driver.db(dbName).get(collectionName);
+},
+
+dBclose: async function () {
+  return await driver.close();
+}
+*/
