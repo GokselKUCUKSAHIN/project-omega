8fcd62f419aa27dac4125f7efb2084a072e2458c
GokselKUCUKSAHIN <gokselkucuksahin@gmail.com>
2022-01-06 11:04:41 +0300

    file to db updated.

--- server/file-to-db/file-to-db.js
+++ server/file-to-db/file-to-db.js
@@ -6,18 +6,16 @@ const {using} = require("using-statement");
 function loadLogs() {
   const dirContent = fs.readdirSync('logs');
   const logs = [];
-  dirContent.filter(v => v.match(/[\w\s\-]\.json$/i)).forEach(dir => {
+  dirContent.filter(v => v.match(/[\w\s\-]\.json$/i)).foEarch(dir => {
     logs.push(JSON.parse(fs.readFileSync(join("logs", dir), "utf-8")));
   });
   return logs;
 }
 
-/*
 (async _ => {
   using(await mongoDbDriverFactory("mongodb://superUser:pass123@10.1.8.88:27017"), async driver => {
-    driver.db("epatch").get("results");
-    const response = await driver.col().insertMany(loadLogs());
+    const collection = driver.db("epatch").get("results");
+    const response = await collection.insertMany(loadLogs());
     console.log(response);
   });
 })();
- */
